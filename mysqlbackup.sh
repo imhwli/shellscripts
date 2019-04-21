9693c2233ba6cf3cbf8e36f7f2ecec7206258698
imhwli <imhwli@outlook.com>
2019-04-20 20:52:17 -0700

    notes

--- mysqlbackup.sh
+++ mysqlbackup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 #
 # Setup up location
 DEST="/home/hello/Desktop/test/"
@@ -9,7 +8,7 @@ DEST="/home/hello/Desktop/test/"
 # Should use key management and dynamic key like Vault
 USERNAME='testuser' 
 PASSWORD='Nono1234$$'
-DBNAMEIP='localhost'
+DBNAME='localhost'
 
 #
 # check if destination exist, if not create it
@@ -24,13 +23,13 @@ fi
 #
 # Creating backup name with absolute path
 #
-BACKUPNAME=$(date+"DATE%Y%m%d-TIME%H%M%S}
-BCKUPPATH="$DEST$BACKUPNAME.db"
+BACKUPNAME=$(date +"DATE%Y%m%d-TIME%H%M%S")
+BCKUPPATH="$DEST$BACKUPNAME.sql"
 
 #
 # check Database connection
 #
-if [ ! mysql --host=$DBNAME --user=$USERNAME --password=$PASSWORD ] 
+ if [ ! -n 'mysql --host=$DBNAME --user=$USERNAME --password=$PASSWORD' ]
 then
 
 	#
@@ -39,19 +38,14 @@ then
 	echo "Cannot connect to database"
 else
 
-fi
-
-
 	#
 	# Backup all databases in MySQL
 	#
-	mysqldump dbname -h <hostname> -u <username> -p > DEST/$BACKUPNAME.sql
+	mysqldump --all-databases --host=$DBNAME --user=$USERNAME --password=$PASSWORD > $BCKUPPATH
 
 fi
 
 # transfer file to Archive 
 # Archieve, encrypt, 
-
-
 
 # mysql -h localhost -u root -p
